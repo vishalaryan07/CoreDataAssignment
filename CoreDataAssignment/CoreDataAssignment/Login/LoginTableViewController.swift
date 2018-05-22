d1ba58abb172fce0940f52648931734312f42090
Vishal Ranjan <vishal.ranjan@tavant.com>
2018-05-22 10:51:51 +0530

    Added alert for login view

--- CoreDataAssignment/CoreDataAssignment/Login/LoginTableViewController.swift
+++ CoreDataAssignment/CoreDataAssignment/Login/LoginTableViewController.swift
@@ -56,6 +56,21 @@ class LoginTableViewController: UITableViewController {
                     appDelegate.routeView()
                 } else {
                     print("error msg: ", error)
+                    let alert = UIAlertController(title: "Alert", message: "Please enter valid credentials", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+                        switch action.style{
+                        case .default:
+                            print("default")
+                            
+                        case .cancel:
+                            print("cancel")
+                            
+                        case .destructive:
+                            print("destructive")
+                            
+                            
+                        }}))
+                    self.present(alert, animated: true, completion: nil)
                 }
             }
         }
